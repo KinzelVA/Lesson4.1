1a46d7d4673d7d0b1ac15b91ff2a9c9970cfea23
KinzelVA <168530439+KinzelVA@users.noreply.github.com>
2024-05-07 21:34:16 +0700

    add test-Branch conf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 a = 7 #создание перменной
 b = 9
-
 print("Я причина конфликта")
 if a > b:
     print("a больше чем b")
@@ -8,7 +7,7 @@ elif b < a:
     print("b больше чем a")
 else:
     print("a равно b")
-
+import math
 print("это нам не нужно")
 
 
